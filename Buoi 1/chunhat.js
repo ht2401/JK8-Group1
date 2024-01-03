91acf16010702cc9097dd20a8ad147297bc1d36c
1Ginz <hunglq2908@gmail.com>
2024-01-03 09:36:42 +0700

    add module 1

---
+++ Buoi 1/chunhat.js
@@ -0,0 +1,8 @@
+let length = (Number)(prompt("nhap chieu dai hinh hop chu nhat"));
+let width = (Number)(prompt("nhap chieu rong hinh hop chu nhat"));
+let height = (Number)(prompt("nhap chieu cao hinh hop chu nhat"));
+
+var answer = (length+width)*2*height + 2 * length * width;
+console.log(answer)
+
+
