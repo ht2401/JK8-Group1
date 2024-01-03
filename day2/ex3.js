ed005d5e33aaa1d053ffec02e4a4f80269d06d1d
admin <hathangemhango@gmail.com>
2024-01-03 16:21:06 +0700

    nop tiep day 2

---
+++ day2/ex3.js
@@ -0,0 +1,13 @@
+function isPrime(number) {
+    if (number < 2) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  
