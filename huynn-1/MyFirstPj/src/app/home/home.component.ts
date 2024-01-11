d564ce75a1af96b1d4cc9eb6793cf891a74e3bf5
huy nguyen <huynn@hivetech.vn>
2024-01-11 14:02:48 +0700

    done logout

--- huynn-1/MyFirstPj/src/app/home/home.component.ts
+++ huynn-1/MyFirstPj/src/app/home/home.component.ts
@@ -4,14 +4,18 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ListOfBookComponent } from './list-of-book/list-of-book.component';
 import { Book } from '../services/interfaces/book';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ListOfBookComponent],
+  imports: [CommonModule, ListOfBookComponent, BsDropdownModule, FontAwesomeModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  public faUser = faUser;
 }
