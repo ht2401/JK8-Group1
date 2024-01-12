9e7963414792b721fb2e1f52f3ad5101e0314541
Đặng Quỳnh Trang <126007365+belemon81@users.noreply.github.com>
2024-01-12 09:13:22 +0700

    Structure modifications

--- trangdq-1/Angular/ProjectNg/src/app/app.module.ts
+++ trangdq-1/Angular/ProjectNg/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AdminDashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
