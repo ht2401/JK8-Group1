a61424616dd7041078e092ee8f3d7220645d3e1e
Đặng Quỳnh Trang <126007365+belemon81@users.noreply.github.com>
2024-01-12 16:17:20 +0700

    Get data for homepage

--- trangdq-1/Angular/ProjectNg/src/app/app.component.ts
+++ trangdq-1/Angular/ProjectNg/src/app/app.component.ts
@@ -7,6 +7,19 @@ export type User = {
 export type ErrorMessage = {
   message?: string;
 }
+export type Book = {
+  id: number,
+  name: string,
+  price: string,
+  description: string,
+  inStock: number,
+  images: Array<string>
+  category: string,
+  discount: number
+}
+
+export const SERVER_URL = "http://localhost:8080"
+
 
 @Component({
   selector: 'app-root',
--- trangdq-1/Angular/ProjectNg/src/app/home/home.component.ts
+++ trangdq-1/Angular/ProjectNg/src/app/home/home.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { SERVER_URL } from '../app.component';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of, take } from 'rxjs';
+
+type Book = {
+  id: number,
+  name: string,
+  price: string,
+  description: string,
+  inStock: number,
+  images: Array<string>
+  category: string,
+  discount: number
+}
 
 @Component({
   selector: 'app-home',
@@ -6,5 +20,21 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  public products: Observable<Array<Book>> = this.getProducts()
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void { 
+  }
+
+  getProducts() {
+    return this.http.get<Array<Book>>(`${SERVER_URL}\0`)
+      .pipe(
+        take(1),  
+        catchError(error => {
+          console.error('Error fetching products:', error); 
+          return []
+        })
+      );
+  }
 
 }
